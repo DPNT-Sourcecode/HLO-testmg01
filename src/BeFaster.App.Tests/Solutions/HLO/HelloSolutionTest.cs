51507e98e798e571f4360a0f254637f2c2473587
sbx_user1051 <sbx_user1051@169.254.54.189>
2024-11-17 17:57:41 +0000

    Sun Nov 17 17:57:41 UTC 2024

--- src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
@@ -6,15 +6,14 @@ namespace BeFaster.App.Tests.Solutions.HLO
     [TestFixture]
     public class HelloSolutionTest
     {
-        [TestCase("", ExpectedResult = "Hello, World!")] // Empty string
-        [TestCase(" ", ExpectedResult = "Hello, World!")] // White space
-        [TestCase("John", ExpectedResult = "Hello, World!")] // Valid name
-        [TestCase("Anne", ExpectedResult = "Hello, World!")] // Another valid name
-        public string Hello_ReturnCorrectGreeting(string friendName)
+        [TestCase("John", ExpectedResult = "Hello, John!")] // Valid name
+        [TestCase("Anne", ExpectedResult = "Hello, Anne!")] // Another valid name
+        public string Hello_ValidInputs_ReturnsGreeting(string friendName)
         {
             return HelloSolution.Hello(friendName);
         }
     }
 }
+
 
 
