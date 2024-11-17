bf56d5a8e72ec098299ec108d452f87be6aa46e6
sbx_user1051 <sbx_user1051@169.254.54.189>
2024-11-17 17:45:00 +0000

    Sun Nov 17 17:45:00 UTC 2024

--- src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
+++ src/BeFaster.App.Tests/Solutions/HLO/HelloSolutionTest.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BeFaster.App.Solutions.HLO;
+using NUnit.Framework;
 
 namespace BeFaster.App.Tests.Solutions.HLO
 {
-    internal class HelloSolutionTest
+    [TestFixture]
+    public class HelloSolutionTest
     {
+        [TestCase("", ExpectedResult = "Hello, World")] // Empty string
+        [TestCase(" ", ExpectedResult = "Hello, World")] // White space
+        [TestCase("John", ExpectedResult = "Hello, John")] // Valid name
+        [TestCase("Anne", ExpectedResult = "Hello, Anne")] // Another valid name
+        public string Hello_ReturnCorrectGreeting(string friendName)
+        {
+            return HelloSolution.Hello(friendName);
+        }
     }
 }
+
